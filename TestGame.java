f8272949cff42cbad5d1708a8be0e91c51aa01a0
StevenRhodes <steve.rhodes@carleton.ca>
2017-08-15 17:03:58 -0400

    Playing Multiple Game to reach max points

--- TestGame.java
+++ TestGame.java
@@ -1,16 +1,44 @@
 
 public class TestGame {
+
+    public static int[] scores = new int[numbPlayers()];
+    public static int numbGames = 3;
+    public static final int WINNING_SCORE = 1000;
+
+    public static int numbPlayers(){
+        if(Crazy8Game.twoPlayer){
+            return 2;
+        }
+        else if (Crazy8Game.threePlayer){
+            return 3;
+        }
+        else
+            return 4;
+    }
+
     public static void main(String[] args){
-        Crazy8Game[] game = new Crazy8Game[20];
-        for (int i =0; i<game.length; i++){
-            game[i] = new Crazy8Game();
+        Crazy8Game[] game = new Crazy8Game[numbGames];
+
+        for (int i =0; i<scores.length; i++){
+            scores[i] = 0;
+        }
+        
+        int gameCount = 0;
+        boolean flag = true;
+        while(flag){
+            System.out.println("This is game: " + (gameCount +1));
+            Crazy8Game.main(null);
+            for (int i =0; i<scores.length; i++){
+                if (scores[i] >= WINNING_SCORE){
+                    flag = false;
+                }
+            }
+            gameCount++;
         }
 
-        for (int i =0; i<game.length; i++){
-
-            System.out.println("\nThis is game " + (i+1) + "\n");
-
-            game[i].main(null);
+        System.out.println("\n-*-*-*-OVERALL RESULTS-*-*-*-\n");
+        for (int i =0; i<scores.length; i++){
+            System.out.println("Player " + i + " Total Points = " + scores[i]);
         }
     }
 }
