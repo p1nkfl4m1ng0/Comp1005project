e5ab881bb476d053bc039147727a9f0f6c103caf
cioboanCarleton <oanamaria.ciobanu@carleton.ca>
2017-08-15 20:13:34 -0400

    New versions of files for MemoryPlayer

--- Crazy8Game_MemoryPlayer.java
+++ Crazy8Game_MemoryPlayer.java
@@ -44,6 +44,7 @@ public class Crazy8Game_MemoryPlayer{
     //PLAY
     ArrayList<Player> people = new ArrayList<Player>(Arrays.asList(players));
     people.get(0).play(discardPile1, drawPile1, people);
+    System.out.println("Last card on discard pile is: " + discardPile1.top());
     System.out.println();
     System.out.println();
     
@@ -71,6 +72,7 @@ public class Crazy8Game_MemoryPlayer{
     //PLAY
    ArrayList<Player> people2 = new ArrayList<Player>(Arrays.asList(players));
     people2.get(0).play(discardPile2, drawPile2, people2);
+    System.out.println("Last card on discard pile is: " + discardPile2.top());
     System.out.println();
     System.out.println();
     
@@ -79,9 +81,9 @@ public class Crazy8Game_MemoryPlayer{
     System.out.println();
     
     // HAND 
-    Card[] testHand3 = {new Card(Card.SUITS[0], Card.RANKS[2]), 
-                       new Card(Card.SUITS[1], Card.RANKS[2]),
-                       new Card(Card.SUITS[2], Card.RANKS[2])};
+    Card[] testHand3 = {new Card(Card.SUITS[0], Card.RANKS[2]),
+                       new Card(Card.SUITS[2], Card.RANKS[2]),
+                       new Card(Card.SUITS[1], Card.RANKS[2])};
                                    
     players[0] = new MemoryPlayer(testHand3);
     System.out.println("MemoryPlayer's starting hand is : " + Arrays.toString(testHand3)); 
@@ -103,15 +105,108 @@ public class Crazy8Game_MemoryPlayer{
     //PLAY
    ArrayList<Player> people3 = new ArrayList<Player>(Arrays.asList(players));
     people3.get(0).play(discardPile3, drawPile3, people3);
+    System.out.println("Last card on discard pile is: " + discardPile3.top());
     System.out.println();
     System.out.println();
     
     /*** Test Case 4: If I have multiple cards of the same suit, I will play the one whose rank has been seen the most.*/
+    System.out.println("Test Case 4: If I have multiple cards of the same suit, I will play the one whose rank has been seen the most.");
+    System.out.println();
+    
+    // HAND 
+    Card[] testHand4 = {new Card(Card.SUITS[0], Card.RANKS[2]),
+                       new Card(Card.SUITS[0], Card.RANKS[3]),
+                       new Card(Card.SUITS[0], Card.RANKS[4])};
+                                   
+    players[0] = new MemoryPlayer(testHand4);
+    System.out.println("MemoryPlayer's starting hand is : " + Arrays.toString(testHand4)); 
+    
+    //DISCARDPILE
+    DiscardPile discardPile4 = new DiscardPile();
+    discardPile4.add(new Card(Card.SUITS[1], Card.RANKS[2]));
+    discardPile4.add(new Card(Card.SUITS[2], Card.RANKS[2]));
+    discardPile4.add(new Card(Card.SUITS[1], Card.RANKS[3]));
+    discardPile4.add(new Card(Card.SUITS[2], Card.RANKS[3]));
+    discardPile4.add(new Card(Card.SUITS[3], Card.RANKS[3]));
+    discardPile4.add(new Card(Card.SUITS[0], Card.RANKS[5]));
+    System.out.println("Last card on discard pile is: " + discardPile4.top());
+    
+    //DRAWPILE
+    Stack<Card> drawPile4 = new Stack<Card>();
+    drawPile4.push(new Card(Card.SUITS[4], Card.RANKS[3]));
+    
+    //PLAY
+   ArrayList<Player> people4 = new ArrayList<Player>(Arrays.asList(players));
+    people4.get(0).play(discardPile4, drawPile4, people4);
+    System.out.println("Last card on discard pile is: " + discardPile4.top());
+    System.out.println();
+    System.out.println();
     
     // Test Case 5: If I have multiple cards that match on rank and suit, play one of the ones that match by rank, and pick
     //         the one whose suit has been seen the most.
+    System.out.println("Test Case 5: If I have multiple cards that match on rank and suit, play one of the ones that match by rank, and pick the one whose suit has been seen the most.");
+    System.out.println();
+    
+    // HAND 
+    Card[] testHand5 = {new Card(Card.SUITS[0], Card.RANKS[2]),
+                       new Card(Card.SUITS[0], Card.RANKS[3]),
+                       new Card(Card.SUITS[2], Card.RANKS[5]),
+                       new Card(Card.SUITS[3], Card.RANKS[5])};
+                                   
+    players[0] = new MemoryPlayer(testHand5);
+    System.out.println("MemoryPlayer's starting hand is : " + Arrays.toString(testHand5)); 
+    
+    //DISCARDPILE
+    DiscardPile discardPile5 = new DiscardPile();
+    discardPile5.add(new Card(Card.SUITS[1], Card.RANKS[2]));
+    discardPile5.add(new Card(Card.SUITS[2], Card.RANKS[2]));
+    discardPile5.add(new Card(Card.SUITS[1], Card.RANKS[3]));
+    discardPile5.add(new Card(Card.SUITS[2], Card.RANKS[3]));
+    discardPile5.add(new Card(Card.SUITS[3], Card.RANKS[3]));
+    discardPile5.add(new Card(Card.SUITS[0], Card.RANKS[5]));
+    System.out.println("Last card on discard pile is: " + discardPile5.top());
+    
+    //DRAWPILE
+    Stack<Card> drawPile5 = new Stack<Card>();
+    drawPile5.push(new Card(Card.SUITS[4], Card.RANKS[3]));
+    
+    //PLAY
+   ArrayList<Player> people5 = new ArrayList<Player>(Arrays.asList(players));
+    people5.get(0).play(discardPile5, drawPile5, people5);
+    System.out.println("Last card on discard pile is: " + discardPile5.top());
+    System.out.println();
+    System.out.println();
     
 
+    // Test Case 6: If I play an 8, change suit to what has been seen most.
+    System.out.println("Test Case 6: If I play an 8, change suit to what has been seen most.");
+    System.out.println();
+    
+    // HAND 
+    Card[] testHand6 = {new Card(Card.SUITS[0], Card.RANKS[2]), 
+                       new Card(Card.SUITS[0], Card.RANKS[3]),
+                       new Card(Card.SUITS[0], Card.RANKS[4])};
+                                   
+    players[0] = new MemoryPlayer(testHand6);
+    System.out.println("MemoryPlayer's starting hand is : " + Arrays.toString(testHand6)); 
+    
+    //DISCARDPILE
+    DiscardPile discardPile6 = new DiscardPile();
+    discardPile6.add(new Card(Card.SUITS[1], Card.RANKS[5]));
+    System.out.println("Last card on discard pile is: " + discardPile6.top());
+    
+    //DRAWPILE
+    Stack<Card> drawPile6 = new Stack<Card>();
+    drawPile6.push(new Card(Card.SUITS[1], Card.RANKS[4]));
+    drawPile6.push(new Card(Card.SUITS[3], Card.RANKS[2]));
+    drawPile6.push(new Card(Card.SUITS[4], Card.RANKS[8]));
+    
+    //PLAY
+    ArrayList<Player> people6 = new ArrayList<Player>(Arrays.asList(players));
+    people6.get(0).play(discardPile6, drawPile6, people6);
+    System.out.println("Last card on discard pile is: " + discardPile6.top());
+    System.out.println();
+    System.out.println();
     
   }
   
--- MemoryPlayer.java
+++ MemoryPlayer.java
@@ -6,15 +6,84 @@ public class MemoryPlayer extends Player {
   
   public MemoryPlayer(Card[] cards){this.hand = new ArrayList<Card>(Arrays.asList(cards));}
   
-  public boolean play(DiscardPile       discardPile, 
-                      Stack<Card>       drawPile, 
-                      ArrayList<Player> players)
-  {
-    boolean playMade = false;
-    System.out.println("MemoryPlayer's hand is: " + hand);
+  public String bestSuit(DiscardPile discardPile) {
+    String bestSuit;
+    System.out.println("An 8 has been played. The player has to decide what suit to switch to.");
+    getcountSuits(discardPile);
     
+    bestSuit = Card.SUITS[2];
+    System.out.println("The player switches the suit to: " + bestSuit);
+    return bestSuit;
+  }
+  
+  public int[] getcountRanks(DiscardPile discardPile) {
+      // Create memory as all cards in the discard pile and player's current hand
+      ArrayList<Card> memory = new ArrayList<Card>(); 
+      while (!discardPile.empty()){
+        memory.add(discardPile.remove());
+      }
+      for (int i=memory.size()-1; i>=0; i--) {
+        discardPile.add(memory.get(i));
+      }
+      for (int i=0; i<hand.size(); i++) {
+        memory.add(hand.get(i));
+      }
+      System.out.println("Looking at cards in memory ... " + memory);
+      
+      //** gen counts of ranks played in memory
+      int[] countRanks = new int[15]; //will contain counts for each rank played.     
+      
+      //look through memory and populate countRanks
+      for (int i=0; i<Card.RANKS.length ; i++) {
+        for (int j=0; j< memory.size(); j++){
+          if (memory.get(j).getRankString()==Card.RANKS[i]) {
+            countRanks[i]++;
+            //System.out.println("countRanks " + i + ": " + countRanks[i]);
+          }
+        }
+      }
+     System.out.println("frequency of ranks seen: " + Arrays.toString(countRanks));
+     return countRanks;
+  }
+  
+  public int[] getcountSuits(DiscardPile discardPile) {
+      // Create memory as all cards in the discard pile and player's current hand
+      ArrayList<Card> memory = new ArrayList<Card>(); 
+      while (!discardPile.empty()){
+        memory.add(discardPile.remove());
+      }
+      for (int i=memory.size()-1; i>=0; i--) {
+        discardPile.add(memory.get(i));
+      }
+      
+      for (int i=0; i<hand.size(); i++) {
+        memory.add(hand.get(i));
+      }
+      System.out.println("Looking at cards in memory ... " + memory);
+      
+      //** gen counts of suits played in memory
+      int[] countSuits = new int[5];  //will contain counts for each suit played.
+      
+      for (int i=0; i<Card.SUITS.length ; i++) {
+        for (int j=0; j< memory.size(); j++){
+          if (memory.get(j).getSuit()==Card.SUITS[i]) {
+            countSuits[i]++;
+          }
+        }
+      }
+      
+      System.out.println("frequency of suits seen: " + Arrays.toString(countSuits));
+      return countSuits;
+  }
+  
+  public boolean play(DiscardPile       discardPile,
+                      Stack<Card>       drawPile,
+                      ArrayList<Player> players) {
+    
+    boolean retrn=false;
     
     // Figure out what cards I have that are playable
+    // Note that I don't consider an 8 as playable off the bat. I'll only play it I have nothing else I can play.
     ArrayList<Card> playable = new ArrayList<Card>();
     for (int i = 0; i < hand.size(); i++) {
       if ((hand.get(i).getSuit() == discardPile.top().getSuit()) || (hand.get(i).getRank() == discardPile.top().getRank())){
@@ -22,84 +91,41 @@ public class MemoryPlayer extends Player {
       }
     }
     System.out.println("playable cards are: " + playable);
-    
-    // If nothing is playable, then draw a card
-    if (playable.size()==0 && drawPile.empty()) { //if nothing is playable, but you have nothing to draw, return true
-      return true;
-    }
-    else if (playable.size()==0 && !drawPile.empty()) {
-      hand.add(drawPile.pop());
-      System.out.println("MemoryPlayer Draws: " + hand.get(hand.size()-1));
-      
-      // If the card you just drew is playable, play it.
-      if ((hand.get(hand.size()-1).getSuit() == discardPile.top().getSuit()) || 
-          (hand.get(hand.size()-1).getRank() == discardPile.top().getRank())) {
-        System.out.println("The card just drawn can be played. MemoryPlayer's play is: " + hand.get(hand.size()-1));
-        discardPile.add(this.hand.remove(hand.size()-1));
-        return false;
-      }
+  
+    // If at most one card is playable, do what the RandomPlayer would do (ie. play that card or keep drawing until you have something to play)
+    if (playable.size()<2) {
+      retrn = playValid(discardPile,drawPile,players);
+      eight(bestSuit(discardPile),discardPile);
+      return retrn;
     }
     
-    // If only one card is playable, play that card
-    else if (playable.size()==1) {
-      System.out.println("Only one card can be played. MemoryPlayer's play is: " + playable.get(0));
-      for (int i=0; i<hand.size(); i++){
-        if (hand.get(i)==playable.get(0)){
-          discardPile.add(this.hand.remove(i));
-        }
-      } 
-    }
-    //**** If more than one card is playable, decide which one is best to play
-    else if (playable.size()>=2) {
+    // If more than one card is playable, decide which one is best to play
+    if (playable.size()>=2) {
       
-      //** gen counts of ranks and suits played in discard pile
-      int[] countRanks = new int[15]; //will contain counts for each rank played.
-      int[] countSuits = new int[5];  //will contain counts for each suit played.
-      
-      //make an arraylist copy of discardPile
-      ArrayList<Card> discardPileMemory = new ArrayList<Card>(); 
-      while (!discardPile.empty()){
-        discardPileMemory.add(discardPile.remove());
-      }
-      
-      for (int i=0; i<discardPileMemory.size(); i++) {
-        discardPile.add(discardPileMemory.get(i));
-      }
-      System.out.println("Multiple cards can be played. discardPileMemory is: " + discardPileMemory);
-      
-      //look through discardPileMemory and populate countRanks and countSuits
-      for (int i=0; i<Card.RANKS.length ; i++) {
-        for (int j=0; j< discardPileMemory.size(); j++){
-          if (discardPileMemory.get(j).getRankString()==Card.RANKS[i]) {
-            countRanks[i]++;
-            //System.out.println("countRanks " + i + ": " + countRanks[i]);
-          }
-        }
-      }
-      for (int i=0; i<Card.SUITS.length ; i++) {
-        for (int j=0; j< discardPileMemory.size(); j++){
-          if (discardPileMemory.get(j).getSuit()==Card.SUITS[i]) {
-            countSuits[i]++;
-            // System.out.println("countSuits: " + i + ": " + countSuits[i]);
-          }
-        }
-      }
-      
-      //**code to decide which card to play
-      
-      //*Look across cards with the same rank
+      // First look across cards with the same rank. If there is at least one card that matches on rank, I will play it.
       ArrayList<Card> playableByRank = new ArrayList<Card>(); //create arraylist of cards with same rank
       for (int i=0; i<playable.size(); i++) {
         if (discardPile.top().getRank()==playable.get(i).getRank()){
           playableByRank.add(playable.get(i));
         }
       }
-      
-      System.out.println("Multiple cards with the same rank can be played. They are: " + playableByRank);
-      
-      if (playableByRank.size()!=0) {
-        ArrayList<Card> cardToPlayByRank = new ArrayList<Card>(); 
-        cardToPlayByRank.add(playableByRank.get(0)); //pick the first card to play in case nothing is better than it.
+      System.out.println("playableByRank " + playableByRank);
+      //If there is only one card with the same rank, play it. 
+      if (playableByRank.size()==1) {
+        Card cardToPlayByRank = new Card(playableByRank.get(0).getSuit(), playableByRank.get(0).getRankString()); 
+        cardToPlayByRank = playableByRank.get(0);
+        hand.remove(cardToPlayByRank);
+        hand.add(0, cardToPlayByRank);
+        retrn = playValid(discardPile,drawPile,players);
+        eight(bestSuit(discardPile),discardPile);
+        return retrn;        
+      }
+      // If there are multiple cards of the same rank, play the one whose suit has been seen the most
+      else if (playableByRank.size()>1) {
+        System.out.println("Multiple cards with the same rank can be played. They are: " + playableByRank);
+        int[] countSuits = getcountSuits(discardPile);
+        Card cardToPlayByRank = new Card(playableByRank.get(0).getSuit(), playableByRank.get(0).getRankString()); 
+        cardToPlayByRank = playableByRank.get(0); //pick the first card to play in case nothing is better than it.
         int highest = 0;
         for (int i=0; i<playableByRank.size(); i++) {
           for (int j=0; j<Card.SUITS.length; j++) {
@@ -107,58 +133,65 @@ public class MemoryPlayer extends Player {
               System.out.println("The suit of this card " + playableByRank.get(i) + " has been seen this many times: " + countSuits[j]);
               
               if (countSuits[j]>=highest) {
-                System.out.println("Best card is that whose suite has been seen the most thusfar: " + countSuits);
-                cardToPlayByRank.add(0, playableByRank.get(i));
+                highest = countSuits[j];
+                System.out.println("Best card thusfar: " + playableByRank.get(i));
+                cardToPlayByRank = playableByRank.get(i);
               }
             }
           }
         }
+        
         System.out.println("Best card to play of all those with same rank: " + cardToPlayByRank);
-        for (int i=0; i<hand.size(); i++){
-          if (hand.get(i)==cardToPlayByRank.get(0)){
-            discardPile.add(this.hand.remove(i));
-          }
-        }
+        hand.remove(cardToPlayByRank);
+        hand.add(0, cardToPlayByRank);
+
+        retrn = playValid(discardPile,drawPile,players);
+        eight(bestSuit(discardPile),discardPile);
+        return retrn;     
       }
       
-      //*Look across cards with same suit
+      
+      // Given there were no cards of the same rank, look across cards with the same suit. 
       ArrayList<Card> playableBySuit = new ArrayList<Card>(); //create arraylist of cards with same suit
       for (int i=0; i<playable.size(); i++) {
         if (discardPile.top().getSuit()==playable.get(i).getSuit()){
           playableBySuit.add(playable.get(i));
         }
       }
-      
-      System.out.println("Multiple cards with the same suit can be played. They are: " + playableBySuit);
-      
-      if (playableBySuit.size()!=0) {
-        ArrayList<Card> cardToPlayBySuit = new ArrayList<Card>();
-        cardToPlayBySuit.add(playableBySuit.get(0)); //pick the first card to play in case nothing is better than it.
+      System.out.println("playableBySuit " + playableBySuit);
+      // If there are multiple cards of the same rank, play the one whose suit has been seen the most
+      if (playableBySuit.size()>1) {
+        System.out.println("Multiple cards with the same suit can be played. They are: " + playableBySuit);
+        int[] countRanks = getcountRanks(discardPile);
+        Card cardToPlayBySuit = new Card(playableBySuit.get(0).getSuit(), playableBySuit.get(0).getRankString()); 
+        cardToPlayBySuit = playableBySuit.get(0); //pick the first card to play in case nothing is better than it.
         int highest = 0;
         for (int i=0; i<playableBySuit.size(); i++) {
-          for (int j=0; j<Card.SUITS.length; j++) {
+          for (int j=0; j<Card.RANKS.length; j++) {
             if (playableBySuit.get(i).getRankString()==Card.RANKS[j]){
               System.out.println("The rank of this card " + playableBySuit.get(i) + " has been seen this many times: " + countRanks[j]);
+              
               if (countRanks[j]>=highest) {
-                System.out.println("Best card is that whose rank has been seen the most thusfar: " + countRanks);
-                cardToPlayBySuit.add(0, playableBySuit.get(i));
+                highest = countRanks[j];
+                //System.out.println("Best card thusfar: " + playableBySuit.get(i));
+                cardToPlayBySuit = playableBySuit.get(i);
               }
             }
           }
         }
+        
         System.out.println("Best card to play of all those with same suit: " + cardToPlayBySuit);
-        for (int i=0; i<hand.size(); i++){
-          if (hand.get(i)==cardToPlayBySuit.get(0)){
-            discardPile.add(this.hand.remove(i));
-          }
-        }
+        hand.remove(cardToPlayBySuit);
+        hand.add(0, cardToPlayBySuit);
+
+        retrn = playValid(discardPile,drawPile,players);
+        eight(bestSuit(discardPile),discardPile);
+        return retrn;     
+        
       }
+      
     }
-    
-    // end game if no more cards in hand or no more cards to pick up from drawPile
-    if( this.hand.size() == 0 || drawPile.empty()){
-      return true;
-    }
-    return false;
+      return retrn;
   }
+   
 }
