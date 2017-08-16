3368a4df056c336e4aca86f3304b9c1bc2a329bb
jatinkhaira <jatsunltd@gmail.com>
2017-08-16 17:15:33 -0400

    Create ExtraCards.java
    jAts file

---
+++ ExtraCards.java
@@ -0,0 +1,19 @@
+import javax.smartcardio.Card;
+
+/**
+ * Created by jatsu on 2017-08-13.
+ */
+public class ExtraCards extends Crazy8Game{
+    //risk taking cards from the draw pile to get power cards
+    //
+    //if the next player only has one 1 card left, keep picking a card until a power card, if they do not have one
+    //already
+    //
+    //no more than one extra card in the early rounds of the game
+    //will not take extra cards if power cards are in hand
+    for(int i = -1; (player[i] =}) ; i += 1){
+        people.get((player - 1 + players.length) % players.length).hand.add(drawPile.pop());
+        //for(getSizeOfHand = powerCard)
+    }
+}
+
